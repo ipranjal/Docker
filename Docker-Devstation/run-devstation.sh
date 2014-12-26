4899e2663c2dc0b829a52e533defa83e15377e94
Mats Engstrom <mats@viacard.com>
2014-12-26 10:09:41 +0000

    Better devstation & run-script

--- Docker-Devstation/run-devstation.sh
+++ Docker-Devstation/run-devstation.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-docker run --interactive --tty --rm --volumes-from=data --link=mysql:mysql --link=redis:redis --volume=/var/run/docker.sock:/var/run/docker.sock viacard/devstation
+docker attach dev
+if [ "$?" == "1" ]; then 
+docker run --interactive --tty --rm --name=dev --volumes-from=data --link=mysql:mysql --link=redis:redis --volume=/var/run/docker.sock:/var/run/docker.sock viacard/devstation
+fi 
 
